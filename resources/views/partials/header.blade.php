57aacfbd280ad9a89131b930140f0bdc6eadf804
ZenRool <lorenzopapperini@hotmail.it>
2022-06-25 02:26:53 +0200

    Header con import e layout

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,41 @@
+<header>
+   <div class="container">
+        <div class="logo">
+            <img src="{{ asset('images/dc-logo.png') }}" alt="">
+        </div>
+        <nav>
+            <ul class="menu">
+                <li>
+                    <a href="">Characters</a>
+                </li>
+                <li class="active">
+                    <a href="">Comics</a>
+                </li>
+                <li>
+                    <a href="">Movies</a>
+                </li>
+                <li>
+                    <a href="">Tv</a>
+                </li>
+                <li>
+                    <a href="">Games</a>
+                </li>
+                <li>
+                    <a href="">Collectivles</a>
+                </li>
+                <li>
+                    <a href="">Videos</a>
+                </li>
+                <li>
+                    <a href="">Fan</a>
+                </li>
+                <li>
+                    <a href="">News</a>
+                </li>
+                <li>
+                    <a href="">Shop</a>
+                </li>
+            </ul>
+        </nav>
+   </div>
+</header> 
