e7985251a860ed58efca6f3f5304b84fe88fbc43
ZenRool <lorenzopapperini@hotmail.it>
2022-06-27 09:33:16 +0200

    Immagine grande nella seconda pagina

--- resources/views/single.blade.php
+++ resources/views/single.blade.php
@@ -3,3 +3,14 @@
 @section("title")
     {{ $comic["title"] }}
 @endsection
+
+@section("main_content")
+    <div class="single">
+        <div class="blue-band"></div>
+        <div class="container">
+            <div class="comic">
+                <img src="{{ $comic['thumb'] }}" alt="">
+            </div>
+        </div>
+    </div>
+@endsection
